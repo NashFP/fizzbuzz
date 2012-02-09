0c24aa1e17a84bff58604be9739cf5f3b1a859f1
calebphillips <caleb.phillips@gmail.com>
2012-02-09 14:51:02 -0600

    Cleaned up.  Turned fizzbuzz into a lazy seq.
    * Used Fn form of div->words mapping, on suggestion from drewr
    * Better names

--- phillipscaleb+clojure/fizzbuzz.clj
+++ phillipscaleb+clojure/fizzbuzz.clj
@@ -2,12 +2,11 @@
 
 (require '[clojure.string :as str])
 
-(defn fizzbuzz [n]
-  (let [divisors (fn [n] (filter #(zero? (mod n %)) [3 5]))
-        mappings {[3 5] "FizzBuzz" [3] "Fizz" [5] "Buzz"}]
-    (str/join ","
-              (for [i (range 1 (inc n))]
-                (get mappings (divisors i) i)))))
+(def fizzbuzz
+  (let [divs (fn [n] (filter #(zero? (mod n %)) [3 5]))
+        divs->words {[3 5] "FizzBuzz" [3] "Fizz" [5] "Buzz"}]
+    (map #(divs->words (divs %) %)
+         (iterate inc 1))))
 
 (assert (= "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz"
-           (fizzbuzz 20)))
+           (str/join "," (take 20 fizzbuzz))))
