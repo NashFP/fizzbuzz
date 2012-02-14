05a94398279e914809189c6fcac773eaa78a9d89
Matt Perkins <ruiml3@gmail.com>
2012-02-13 21:35:51 -0600

    Simple Racket 101 Week 2 Iterative Recursion

--- mspspeak+racket/fizzbuzz.rkt
+++ mspspeak+racket/fizzbuzz.rkt
@@ -1,26 +1,30 @@
 #lang racket
 
 ;Going to commit these in steps to see how you get from point a to b. 
-;The simple plain recursive method  ... 
-;the most remedial week 1 lispy fizz buzz probably.  
-;Definitely not as learned as Calvin's 
+;The slightly more advanced recursive method, using an iterative recursion 
+;Perhaps a week 2 approach
+;again, Definitely not as learned as Calvin's 
 (define (fizzbuzz-from-n-to-k n k)
-  ;simple 0 mod test
   (define (no-remainder? x y)
     (= (modulo x y) 0))
-  ;very simple cond (think switch)...
-  ;with a collection of predicates and response values
   (define (get-x-fizz-or-buzz x)
     (cond ((no-remainder? x 15) "fizzbuzz")
           ((no-remainder? x 5) "buzz")
           ((no-remainder? x 3) "fizz")
           (else (number->string x))))
-  ;the actual recursive algorithm: if predicate, end result, else unfinished recursive call
-  (if (= n k)
-      (get-x-fizz-or-buzz n)
-      (string-append 
-       (get-x-fizz-or-buzz n)       
-       (fizzbuzz-from-n-to-k (+ n 1) k))) ) 
+  ;now we've got another inner procedure that captures the recursive algorithm
+  ;but this time we pass along the string
+  (define (fz-bz-iter s n k)
+    (if (= n k)
+      (string-append s (get-x-fizz-or-buzz n))
+      (fz-bz-iter (string-append s (get-x-fizz-or-buzz n))
+                  (+ n 1)
+                  k)))
+  (fz-bz-iter (get-x-fizz-or-buzz n) (+ n 1) k))
+
+
+
+
 
 ;our test here should return #t for true
 (string=? (fizzbuzz-from-n-to-k 1 100)
