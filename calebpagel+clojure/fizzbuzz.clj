05ae015078fe37a1d803d4769675f883f19e1b58
calebpagel <pagelcj@NLOFRW69D1BP1.dhcp.mc.vanderbilt.edu>
2013-08-15 16:05:32 -0500

    Learning clojure

---
+++ calebpagel+clojure/fizzbuzz.clj
@@ -0,0 +1,27 @@
+;; Learning Clojure ....
+
+;; 
+
+;; Comments welcome
+
+
+
+
+(def buzz (fn [x] (= (mod x 5) 0)))
+
+
+
+(def fizz (fn [x] (= (mod x 3) 0)))
+
+
+
+
+(def fizzbuzzer (fn [x] (if (and (fizz x) (buzz x)) "FIZZBUZZ"
+                          				(if (fizz x) "FIZZ" 
+                            
+					(if (buzz x) "BUZZ" 
+                              						x)))))
+   
+                       
+
+(dotimes [n 100] (println n "is" (fizzbuzzer n)))
