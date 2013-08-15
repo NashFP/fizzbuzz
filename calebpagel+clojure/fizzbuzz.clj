b3eddfea9ae0c95c3634b28bc332989319dcde73
calebpagel <calebpagel@gmail.com>
2013-08-15 16:31:01 -0500

    Tidying up code

--- calebpagel+clojure/fizzbuzz.clj
+++ calebpagel+clojure/fizzbuzz.clj
@@ -5,23 +5,15 @@
 ;; Comments welcome
 
 
-
-
 (def buzz (fn [x] (= (mod x 5) 0)))
-
 
 
 (def fizz (fn [x] (= (mod x 3) 0)))
 
 
-
-
 (def fizzbuzzer (fn [x] (if (and (fizz x) (buzz x)) "FIZZBUZZ"
                           				(if (fizz x) "FIZZ" 
-                            
-					(if (buzz x) "BUZZ" 
+								(if (buzz x) "BUZZ" 
                               						x)))))
-   
-                       
 
 (dotimes [n 100] (println n "is" (fizzbuzzer n)))
