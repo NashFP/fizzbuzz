2989f56d325fc4a5eff0207f8c2746cb2a95cad6
Karlkim Suwamongkol <karlkim@outlook.com>
2013-09-19 22:06:28 -0500

    fsharp newbie solution :-)

---
+++ kimsk+fsharp/fizzbuzz.fsx
@@ -0,0 +1,16 @@
+﻿let fizz x =
+    if x%3 = 0 then true else false
+
+let buzz x =
+    if x%5 = 0 then true else false
+
+let fizbuzz x =
+    match (fizz x), (buzz x), x with
+    | true, true, _ -> "FizzBuzz"
+    | true, _ , _ -> "Fizz"
+    | _, true, _ -> "Buzz"
+    | _, _, x -> string x
+
+{1..20} |> Seq.map fizbuzz |> Seq.toArray
+
+
